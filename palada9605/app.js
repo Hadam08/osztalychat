95cfd3f65a3bfea60db7ec942820faebf762097d
Hadam08 <pallhorvathadam@gmail.com>
2024-12-03 13:33:48 +0100

    added input and struggled with event handler

---
+++ palada9605/app.js
@@ -0,0 +1,11 @@
+function displayText() {
+    var chat = document.getElementById("chat").value
+    var state = window.addEventListener('keydown',(event) => {
+        console.log(event)
+    })
+    if(event.Enter)
+       { alert(chat)
+       }
+    }
+ 
+      
